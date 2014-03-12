73893ac80c45f42e141c5572fb4ec946632657cc
Antony Denyer <antonydenyer@gmail.com>
2014-03-12 10:54:01 +0000

    returns 2 for third fibonacci number - failing test

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -9,4 +9,11 @@ describe "fibonacci generator" do
     @fib.calculate(1).should eq 1
   end
 
+  it "returns 1 for the second fibonacci number" do
+    @fib.calculate(2).should eq 1
+  end
+
+  it "returns 2 for the third fibonacci number" do
+    @fib.calculate(3).should eq 2
+  end
 end
